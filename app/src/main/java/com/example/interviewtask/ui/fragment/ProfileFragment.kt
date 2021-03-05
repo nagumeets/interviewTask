f4ad3a07fecfb7b92fb026c8dc0d9e2a538eaee1
nagumeets <nagumeets@gmail.com>
2021-03-05 20:30:13 +0530

    fixed backstack and reload the recyclerview

--- app/src/main/java/com/example/interviewtask/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/interviewtask/ui/fragment/ProfileFragment.kt
@@ -1,6 +1,5 @@
 package com.example.interviewtask.ui.fragment
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.interviewtask.databinding.FragmentProfileBinding
 
-class SettingsFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
